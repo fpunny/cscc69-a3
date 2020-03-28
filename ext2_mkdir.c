2880f5c9e9be337ccbba99522cedb9e0760a545e
Ralph Maamari <ralphpal@hotmail.com>
2020-03-28 16:37:30 -0400

    Update ext2_mkdir.c

--- ext2_mkdir.c
+++ ext2_mkdir.c
@@ -13,7 +13,21 @@ if the specified directory already exists,
 	then your program should return the appropriate error (ENOENT or EEXIST).
 Again, please read the specifications to make sure you're implementing everything correctly 
 (e.g., directory entries should be aligned to 4B, entry names are not null-terminated, etc.).
+
+Helpful:
+We need this for init
+struct ext2_dir_entry_2 {
+	unsigned int   inode;     /* Inode number = use get_inode function
+	unsigned short rec_len;   /* Directory entry length = EXT2 BLock
+	unsigned char  name_len;  /* Name length = argc length
+	unsigned char  file_type; = #define    EXT2_FT_DIR      2    /* Directory File
+	char           name[];    /* File name, up to EXT2_NAME_LEN = argc
+};
+
 */
 int main(int argc, char *argv[]) {
+	/* Check if path does not exist by calling Navigate, if null return ENOENT */
+	/* Check if specified directory already exists by calling find_file, if true return EEXIST */
+	/* At this point, the path exists and the specified directly doesn't exist yet so we will create it */
 	exit(0);
 }
