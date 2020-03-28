85fdbe3b3605d48c1297bd5bb14828078a72d7fe
Ralph Maamari <ralphpal@hotmail.com>
2020-03-28 17:59:28 -0400

    Update ext2_ln.c

--- ext2_ln.c
+++ ext2_ln.c
@@ -21,6 +21,31 @@ unsigned int   i_block[15];   /* Pointers to blocks
 unsigned short i_links_count; /* Links count
 #define    EXT2_S_IFLNK  0xA000    /* symbolic link
 #define    EXT2_FT_SYMLINK  7    /* Symbolic Link
+
+Details:
+ Links
+Unix filesystems implement the concept of link. Several names can be associated with a inode. 
+The inode contains a field containing the number associated with the file. 
+Adding a link simply consists in creating a directory entry, 
+where the inode number points to the inode, 
+and in incrementing the links count in the inode. When a link is deleted, 
+i.e.when one uses the rm command to remove a filename, 
+the kernel decrements the links count and deallocates the inode if this count becomes zero.
+
+This type of link is called a hard link and can only be used within a single filesystem: 
+it is impossible to create cross-filesystem hard links. 
+Moreover, hard links can only point on files: a directory hard link cannot be created to prevent the apparition of a 
+cycle in the directory tree.
+
+Another kind of links exists in most Unix filesystems. 
+Symbolic links are simply files which contain a filename. 
+When the kernel encounters a symbolic link during a pathname to inode conversion, 
+it replaces the name of the link by its contents, i.e. the name of the target file, and restarts the pathname interpretation. 
+Since a symbolic link does not point to an inode, it is possible to create cross-filesystems symbolic links. 
+Symbolic links can point to any type of file, even on nonexistent files. 
+Symbolic links are very useful because they don't have the limitations associated to hard links. However, they use some disk space,
+allocated for their inode and their data blocks, and cause an overhead in the pathname to inode conversion because the kernel has to
+restart the name interpretation when it encounters a symbolic link.
 */
 int main(int argc, char *argv[]) {
 	/* Check if source file does not exist, if true, return ENOENT */
